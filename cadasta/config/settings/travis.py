16e2c2577d83b88820e679ca0f152365ed250e85
Oliver Roick <oliver.roick@gmail.com>
2015-12-04 16:02:20 +0000

    Standard Travis Setup

---
+++ cadasta/config/settings/travis.py
@@ -0,0 +1,16 @@
+from .default import *
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cadasta',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+    }
+}
