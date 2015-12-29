2e85ae016e71c86b0e3d51fe58b16ede9bf7a995
Oliver Roick <oliver.roick@gmail.com>
2015-12-29 18:43:46 +0000

    Dev Email server settings

--- cadasta/config/settings/dev.py
+++ cadasta/config/settings/dev.py
@@ -40,3 +40,11 @@ DEVSERVER_MODULES = (
     # show django session information
     # 'devserver.modules.request.SessionInfoModule',
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'testing@example.com'
