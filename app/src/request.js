da5ce2b22fd9a3155b00ea89fc1d9ac011cf25d5
Oliver Roick <oliver.roick@gmail.com>
2016-01-07 15:43:49 +0000

    Rewrite HTTP wrapper
    It wraps XMLHttpRequest in a Promise. The Promise is resolved upon
    successful requests (HTTP code 200 - 299) and rejected otherwise.
    
    Use:
    
    ```
    return Request.get('/')
      .then(
        (success => handleSuccess(success),
        (error => handleError(error))
      )
    ```

--- app/src/request.js
+++ app/src/request.js
@@ -1,64 +1,62 @@
 import fetch from 'isomorphic-fetch';
-import { polyfill } from 'es6-promise'; polyfill();
+import { Promise } from 'es6-promise';
 
 import SETTINGS from './settings';
 
-function request(url, method, callback, body={}, authenticate=true) {
-  let fetchParams = {
-    method: method,
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-  };
+function request(url, method, body={}, authenticate=true) {
+  const promise = new Promise(function(resolve, reject) {
+    const client = new XMLHttpRequest();
 
-  if (authenticate) {
-    fetchParams.headers.Authorization = 'Token ' + window.localStorage.auth_token
-  }
+    client.open(method, SETTINGS.API_BASE + url);
 
-  if (body) {
-    fetchParams.body = JSON.stringify(body)
-  }
+    client.setRequestHeader('Accept', 'application/json');
+    client.setRequestHeader('Content-Type', 'application/json');
 
-  return fetch(SETTINGS.API_BASE + url, fetchParams)
-    .then(response => {
-      if (response.status >= 400) {
-        response.json().then(error => {
-          callback({
-            success: false,
-            content: error
-          })
-        })
-      } else {
-        response.text().then(response => {
-          callback({
-            success: true,
-            content: JSON.parse(response || '{}')
-          })
-        })
+    if (authenticate) {
+      client.setRequestHeader('Authorization', 'Token ' + window.localStorage.auth_token);
+    }
+
+    client.send(JSON.stringify(body));
+
+    client.onload = function () {
+      let response;
+      if (this.response) {
+        response = JSON.parse(this.response);  
       }
-    });
+      
+      if (this.status >= 200 && this.status < 300) {
+        resolve(response);
+      } else {
+        reject(response);
+      }
+    };
+
+    client.onerror = function () {
+      reject(this.statusText);
+    };
+  });
+  return promise;
 }
 
 const Request = {
-  get: function(url, callback, authenticate) {
-    return request(url, 'GET', callback, null, authenticate);
+  get: function(url, authenticate) {
+    return request(url, 'GET', null, authenticate);
   },
 
-  delete: function(url, callback, authenticate) {
-    return request(url, 'DELETE', callback, null, authenticate);
+  delete: function(url, authenticate) {
+    return request(url, 'DELETE', null, authenticate);
   },
 
-  post: function(url, callback, body, authenticate) {
-    return request(url, 'POST', callback, body, authenticate);
+  post: function(url, body, authenticate) {
+    return request(url, 'POST', body, authenticate);
   },
 
-  put: function(url, callback, body, authenticate) {
-    return request(url, 'PUT', callback, body, authenticate);
+  put: function(url, body, authenticate) {
+    return request(url, 'PUT', body, authenticate);
   },
 
-  patch: function(url, callback, body, authenticate) {
-    return request(url, 'PATCH', callback, body, authenticate);
+  patch: function(url, body, authenticate) {
+    return request(url, 'PATCH', body, authenticate);
   }
 }
 
--- app/test/test_request.js
+++ app/test/test_request.js
@@ -1,133 +1,92 @@
-import {expect} from 'chai';
+import chai, {expect, assert} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import nock from 'nock';
 
 import SETTINGS from '../src/settings';
 import Request from '../src/request';
 
+chai.use(chaiAsPromised);
+
 describe('request', () => {
   it("sends a GET request", () => {
     const response = { some: "Response" }
-    const expectedResponse = {
-      success: true,
-      content: response
-    }
 
     nock(SETTINGS.API_BASE)
       .get('/')
       .reply(200, response)
 
-    return Request.get(
-      '/',
-      (json => expect(json).to.deep.equal(expectedResponse))
-    );
+    return Request.get('/')
+      .then(
+        (success => expect(success).to.deep.equal(response)),
+        (error => assert(false, "Error called with: " + JSON.stringify(error)))
+      )
   });
 
   it("handles HTTP errors", () => {
     const response = { some: "Error" }
-    const expectedResponse = {
-      success: false,
-      content: response
-    }
 
     nock(SETTINGS.API_BASE)
       .get('/')
       .reply(400, response)
 
-    return Request.get('/',
-      (json => {
-        expect(json).to.deep.equal(expectedResponse)
-      })
-    );
-  });
-
-  it("processes an empty response", () => {
-    const expectedResponse = {
-      success: true,
-      content: {}
-    }
-
-    nock(SETTINGS.API_BASE)
-      .get('/')
-      .reply(200, null)
-
-    return Request.get('/',
-      (json => {
-        expect(json).to.deep.equal(expectedResponse)
-      })
-    );
+    return Request.get('/')
+      .then(
+        (success => assert(false, "Success called with: " + JSON.stringify(success))),
+        (error => expect(error).to.deep.equal(response))
+      )
   });
 
   it("sends a POST request", () => {
     const response = { some: "Response" }
-    const expectedResponse = {
-      success: true,
-      content: response
-    }
 
     nock(SETTINGS.API_BASE)
       .post('/', {})
       .reply(200, response)
 
-    return Request.post('/',
-      (json => {
-        expect(json).to.deep.equal(expectedResponse)
-      }),
-      {}
-    );
+    return Request.post('/', {})
+      .then(
+        (json => expect(json).to.deep.equal(response)),
+        (error => assert(false, "Error called with: " + JSON.stringify(error)))
+      );
   });
 
   it("sends a PUT request", () => {
     const response = { some: "Response" }
-    const expectedResponse = {
-      success: true,
-      content: response
-    }
 
     nock(SETTINGS.API_BASE)
       .put('/', {})
       .reply(200, response)
 
-    return Request.put('/',
-      (json => {
-        expect(json).to.deep.equal(expectedResponse)
-      }),
-      {}
-    );
+    return Request.put('/', {})
+      .then(
+        (json => expect(json).to.deep.equal(response)),
+        (error => assert(false, "Error called with: " + JSON.stringify(error)))
+      );
   });
 
   it("sends a PATCH request", () => {
     const response = { some: "Response" }
-    const expectedResponse = {
-      success: true,
-      content: response
-    }
 
     nock(SETTINGS.API_BASE)
       .patch('/', {})
       .reply(200, response)
 
-    return Request.patch('/',
-      (json => {
-        expect(json).to.deep.equal(expectedResponse)
-      }),
-      {}
-    );
+    return Request.patch('/', {})
+      .then(
+        (json => expect(json).to.deep.equal(response)),
+        (error => assert(false, "Error called with: " + JSON.stringify(error)))
+      );
   });
 
   it("sends a DELETE request", () => {
-    const expectedResponse = {
-      success: true,
-      content: {}
-    }
-
     nock(SETTINGS.API_BASE)
       .delete('/')
       .reply(204)
 
-    return Request.delete('/',
-      (json => {
-        expect(json).to.deep.equal(expectedResponse)
-      })
-    );
+    return Request.delete('/')
+      .then(
+        (json => expect(json).to.be.undefined),
+        (error => assert(false, "Error called with: " + JSON.stringify(error)))
+      );
   });
 });
