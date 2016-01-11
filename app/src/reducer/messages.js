f6247b21e086c177036dd2c80215f6aed43281e8
Oliver Roick <oliver.roick@gmail.com>
2016-01-11 14:32:25 +0000

    Handle success states

--- app/src/reducer/messages.js
+++ app/src/reducer/messages.js
@@ -1,50 +1,64 @@
 import { Map, List } from 'immutable';
 import { createMessage } from '../utils/messages';
 
-const defaultState = Map({
+const DEFAULT_STATE = Map({
   requestsPending: 0,
   userFeedback: List([])
 });
 
-export default function messages(state = defaultState, action) {
-  switch (action.type) {
+const ERROR_MESSAGES = {
+  POST_LOGIN_ERROR: "Unable to login.",
+  POST_LOGOUT_ERROR: "Unable to logout.",
+  POST_CHANGEPASSWORD_ERROR: "Unable to change password.",
+  POST_REGISTER_ERROR: "Unable to register with provided credentials.",
+  POST_UPDATEPROFILE_ERROR: "Unable to update profile.",
+  GET_USERINFO_ERROR: "Unable to get user profile information from server.",
+  POST_RESETPASSWORD_ERROR: "Unable to reset password.",
+  POST_RESETCONFIRMPASSWORD_ERROR: "Unable to reset password.",
+  GET_ACTIVATE_ERROR: "Unable to activate account."
+}
 
-    case 'POST_LOGIN_ERROR':
-      var message = createMessage('error', "Unable to login.", action.response)
-      var userFeedback = state.get('userFeedback').push(message);
-      return state.set('userFeedback', userFeedback);
+const SUCCESS_MESSAGES = {
+  POST_LOGIN_SUCCESS: "Successfully logged in.",
+  POST_LOGOUT_SUCCESS: "Successfully logged out.",
+  POST_CHANGEPASSWORD_SUCCESS: "Successfully changed password.",
+  POST_REGISTER_SUCCESS: "Successfully registered. You can now log in.",
+  POST_UPDATEPROFILE_SUCCESS: "Successfully updated profile information.",
+  POST_RESETPASSWORD_SUCCESS: "Password successfully reset. You have recieved an email to confirm the reset.",
+  POST_RESETCONFIRMPASSWORD_SUCCESS: "Password successfully reset.",
+  GET_ACTIVATE_SUCCESS: "Account successfully activated."
+}
 
-    case 'POST_CHANGEPASSWORD_ERROR':
-      var message = createMessage('error', "Unable to change password", action.response)
+export default function messages(state = DEFAULT_STATE, action) {
+  if (action.type in SUCCESS_MESSAGES) {
+  
+    var message = createMessage('success', SUCCESS_MESSAGES[action.type])
+    var userFeedback = state.get('userFeedback').push(message);
+    return state.set('userFeedback', userFeedback);
 
-      var userFeedback = state.get('userFeedback').push(message);
-      return state.set('userFeedback', userFeedback);
+  } else if (action.type in ERROR_MESSAGES) {
+  
+    var message = createMessage('error', ERROR_MESSAGES[action.type], action.response)
+    var userFeedback = state.get('userFeedback').push(message);
+    return state.set('userFeedback', userFeedback);
+  
+  } else {
 
-    case 'POST_REGISTER_ERROR':
-      var message = createMessage('error', "Unable to register with provided credentials.", action.response)
+    switch (action.type) {
 
-      var userFeedback = state.get('userFeedback').push(message);
-      return state.set('userFeedback', userFeedback);
+      case 'REQUEST_START':
+        var requestsPending = state.get('requestsPending');
+        return state.set('requestsPending', requestsPending + 1);
 
-    case 'POST_UPDATEPROFILE_ERROR':
-      var message = createMessage('error', "Unable to update profile", action.response)
+      case 'REQUEST_DONE':
+        var requestsPending = state.get('requestsPending');
+        return state.set('requestsPending', requestsPending - 1);
 
-      var userFeedback = state.get('userFeedback').push(message);
-      return state.set('userFeedback', userFeedback);
+      case 'DISMISS_MESSAGES':
+        var userFeedback = state.get('userFeedback').clear();
+        return state.merge({ userFeedback });
+    }
 
-    case 'REQUEST_START':
-      var requestsPending = state.get('requestsPending');
-      return state.set('requestsPending', requestsPending + 1);
-
-    case 'REQUEST_DONE':
-      var requestsPending = state.get('requestsPending');
-      return state.set('requestsPending', requestsPending - 1);
-
-    case 'DISMISS_MESSAGES':
-      var userFeedback = state.get('userFeedback').clear();
-      return state.merge({ userFeedback });
-    
-    default:
-      return state
   }
+  return state;
 }
--- app/test/reducer/test_messages.js
+++ app/test/reducer/test_messages.js
@@ -40,6 +40,20 @@ describe('messages reducer', () => {
     expect(requestsPending).to.equal(0);
   });
 
+  it('handles POST_LOGIN_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_LOGIN_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Successfully logged in."
+      }]
+    }));
+  });
+
   it('handles POST_LOGIN_ERROR', () => {
     const state = Map({ userFeedback: List([]) });
 
@@ -58,6 +72,56 @@ describe('messages reducer', () => {
         details: [
           "Unable to login with provided credentials."
         ]
+      }]
+    }));
+  });
+
+  it('handles POST_LOGOUT_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_LOGOUT_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Successfully logged out."
+      }]
+    }));
+  });
+
+  it('handles POST_LOGOUT_ERROR', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = {
+      type: 'POST_LOGOUT_ERROR',
+      response: {
+        "non_field_errors": ["Unable to logout with provided credentials."]
+      }
+    };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'error',
+        msg: "Unable to logout.",
+        details: [
+          "Unable to logout with provided credentials."
+        ]
+      }]
+    }));
+  });
+
+  it('handles POST_REGISTER_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_REGISTER_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Successfully registered. You can now log in."
       }]
     }));
   });
@@ -86,6 +150,20 @@ describe('messages reducer', () => {
     }));
   });
 
+  it('handles POST_CHANGEPASSWORD_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_CHANGEPASSWORD_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Successfully changed password."
+      }]
+    }));
+  });
+
   it("handles POST_CHANGEPASSWORD_ERROR", () => {
     const state = Map({ userFeedback: List([]) });
 
@@ -100,10 +178,24 @@ describe('messages reducer', () => {
     expect(nextState).to.equal(fromJS({
       userFeedback: [{
         type: 'error',
-        msg: "Unable to change password",
+        msg: "Unable to change password.",
         details: [
           "Invalid password."
         ]
+      }]
+    }));
+  });
+
+  it('handles POST_UPDATEPROFILE_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_UPDATEPROFILE_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Successfully updated profile information."
       }]
     }));
   });
@@ -122,9 +214,139 @@ describe('messages reducer', () => {
     expect(nextState).to.equal(fromJS({
       userFeedback: [{
         type: 'error',
-        msg: "Unable to update profile",
+        msg: "Unable to update profile.",
         details: [
           "Another user is already registered with this email address"
+        ]
+      }]
+    }));
+  });
+
+  it('handles GET_USERINFO_ERROR', () => {
+    const state = fromJS({ userFeedback: [] });
+
+    const action = {
+      type: 'GET_USERINFO_ERROR',
+      response: {
+        "email": ["User not found"]
+      }
+    };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'error',
+        msg: "Unable to get user profile information from server.",
+        details: [
+          "User not found"
+        ]
+      }]
+    }));
+  });
+
+  it('handles POST_RESETPASSWORD_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_RESETPASSWORD_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Password successfully reset. You have recieved an email to confirm the reset."
+      }]
+    }));
+  });
+
+  it('handles POST_RESETPASSWORD_ERROR', () => {
+    const state = fromJS({ userFeedback: [] });
+
+    const action = {
+      type: 'POST_RESETPASSWORD_ERROR',
+      response: {
+        "email": ["User not found"]
+      }
+    };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'error',
+        msg: "Unable to reset password.",
+        details: [
+          "User not found"
+        ]
+      }]
+    }));
+  });
+
+  it('handles POST_RESETCONFIRMPASSWORD_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'POST_RESETCONFIRMPASSWORD_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Password successfully reset."
+      }]
+    }));
+  });
+
+  it('handles POST_RESETCONFIRMPASSWORD_ERROR', () => {
+    const state = fromJS({ userFeedback: [] });
+
+    const action = {
+      type: 'POST_RESETCONFIRMPASSWORD_ERROR',
+      response: {
+        "email": ["User not found"]
+      }
+    };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'error',
+        msg: "Unable to reset password.",
+        details: [
+          "User not found"
+        ]
+      }]
+    }));
+  });
+
+  it('handles GET_ACTIVATE_SUCCESS', () => {
+    const state = Map({ userFeedback: List([]) });
+
+    const action = { type: 'GET_ACTIVATE_SUCCESS' };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'success',
+        msg: "Account successfully activated."
+      }]
+    }));
+  });
+
+  it('handles GET_ACTIVATE_ERROR', () => {
+    const state = fromJS({ userFeedback: [] });
+
+    const action = {
+      type: 'GET_ACTIVATE_ERROR',
+      response: {
+        "email": ["User not found"]
+      }
+    };
+    const nextState = messages(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      userFeedback: [{
+        type: 'error',
+        msg: "Unable to activate account.",
+        details: [
+          "User not found"
         ]
       }]
     }));
