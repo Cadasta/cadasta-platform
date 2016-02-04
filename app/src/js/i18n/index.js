04f36de21c849589f92de2c46f8afaf354ad2fc6
Oliver Roick <oliver.roick@gmail.com>
2016-02-04 13:03:20 +0000

    Add component template handling and tests

--- app/src/js/i18n/index.js
+++ app/src/js/i18n/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Jed from 'jed';
 import { sprintf } from 'sprintf-js';
 
@@ -31,6 +32,90 @@ export function setLocale(locale) {
 }
 
 setLocale(browserLocale);
+
+function parseComponentTemplate(string) {
+  const rv = {};
+
+  function process(startPos, group, inGroup) {
+    const regex = /\[(.*?)(:|\])|\]/g;
+    let match;
+    const buf = [];
+    let satisfied = false;
+
+    let pos = regex.lastIndex = startPos;
+    while ((match = regex.exec(string)) !== null) { // eslint-disable-line no-cond-assign
+      const substr = string.substr(pos, match.index - pos);
+      if (substr !== '') {
+        buf.push(substr);
+      }
+
+      if (match[0] === ']') {
+        if (inGroup) {
+          satisfied = true;
+          break;
+        } else {
+          pos = regex.lastIndex;
+          continue;
+        }
+      }
+
+      if (match[2] === ']') {
+        pos = regex.lastIndex;
+      } else {
+        pos = regex.lastIndex = process(regex.lastIndex, match[1], true);
+      }
+      buf.push({ group: match[1] });
+    }
+
+    let endPos = regex.lastIndex;
+    if (!satisfied) {
+      const rest = string.substr(pos);
+      if (rest) {
+        buf.push(rest);
+      }
+      endPos = string.length;
+    }
+
+    rv[group] = buf;
+    return endPos;
+  }
+
+  process(0, 'root', false);
+
+  return rv;
+}
+
+function renderComponentTemplate(template, components) {
+  let idx = 0;
+  function renderGroup(group) {
+    const children = [];
+
+    (template[group] || []).forEach((item) => {
+      if (typeof item === 'string') {
+        children.push(<span key={idx++}>{item}</span>);
+      } else {
+        children.push(renderGroup(item.group));
+      }
+    });
+
+    // in case we cannot find our component, we call back to an empty
+    // span so that stuff shows up at least.
+    let reference = components[group] || <span key={idx++} />;
+    if (!React.isValidElement(reference)) {
+      reference = <span key={idx++}>{reference}</span>;
+    }
+
+    let component;
+    if (children.length > 0) {
+      component = React.cloneElement(reference, { key: idx++ }, children);
+    } else {
+      component = React.cloneElement(reference, { key: idx++ });
+    }
+    return component;
+  }
+
+  return renderGroup('root');
+}
 
 function mark(str) {
   if (!LOCALE_DEBUG) {
@@ -69,4 +154,9 @@ export function t(str, ...args) {
 
 export function tn(singular, plural, ...args) {
   return mark(sprintf(jed.ngettext(singular, plural, args[0] || 0), args));
+}
+
+export function tct(template, components) {
+  const tmpl = parseComponentTemplate(jed.gettext(template));
+  return mark(renderComponentTemplate(tmpl, components));
 }
