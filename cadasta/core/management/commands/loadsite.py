7e0757d62f73aa7154669623e3559f407191e529
Lindsey Jacks <linzjax@gmail.com>
2016-07-11 16:53:39 -0400

    Fixes #387: new command to replace default site object
    added loadeverything command

---
+++ cadasta/core/management/commands/loadsite.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.sites.models import Site
+
+
+class Command(BaseCommand):
+    help = """Loads initial site object for Cadasta.org."""
+
+    def handle(self, *args, **options):
+        if not Site.objects.filter(name='Cadasta').exists():
+            site = Site.objects.get(name='example.com')
+            site.name = 'Cadasta'
+            site.domain = 'platform.cadasta.org'
+            site.save()
+            return "Successfully loaded Cadasta.org object"
+        else:
+            return "Cadasta.org object already exists."
