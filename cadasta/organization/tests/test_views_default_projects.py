5aa5a6691c06a716cb7f26723ca00dd07a9bd6dc
manoramahp <manorama@wso2.com>
2016-09-20 15:45:43 +0530

    fixing test failure : too lengthy lines

--- cadasta/organization/tests/test_views_default_projects.py
+++ cadasta/organization/tests/test_views_default_projects.py
@@ -255,9 +255,10 @@ class ProjectDashboardTest(ViewTestCase, UserTestCase, TestCase):
         )
         response = self.request(user=self.user)
         assert response.status_code == 200
-        assert response.content == self.render_content(is_administrator=True,
-                                            is_allowed_add_location=True,
-                                            is_allowed_add_resource=True)
+        assert response.content == self.render_content(
+            is_administrator=True,
+            is_allowed_add_location=True,
+            is_allowed_add_resource=True)
 
     def test_get_non_existent_project(self):
         with pytest.raises(Http404):
@@ -394,10 +395,10 @@ class ProjectDashboardTest(ViewTestCase, UserTestCase, TestCase):
         self.project.save()
         response = self.request(user=org_admin)
         assert response.status_code == 200
-        assert response.content == self.render_content(is_administrator=True,
-                                        is_allowed_add_location=True,
-                                        is_allowed_add_resource=True
-        )
+        assert response.content == self.render_content(
+            is_administrator=True,
+            is_allowed_add_location=True,
+            is_allowed_add_resource=True)
 
 
 @pytest.mark.usefixtures('make_dirs')
