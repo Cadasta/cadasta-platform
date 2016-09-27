09fc8382ce9d1da766bf2cb480b5c9fc3ccecb5d
manoramahp <manorama@wso2.com>
2016-09-27 16:41:33 +0530

    Revert "Show member list only to an organization member : https://github.com/… (#728)"
    This reverts commit 1b03ef8d0700264f6aa1378304625f54084c954b.

--- cadasta/organization/tests/test_views_default_organizations.py
+++ cadasta/organization/tests/test_views_default_organizations.py
@@ -201,8 +201,7 @@ class OrganizationDashboardTest(ViewTestCase, UserTestCase, TestCase):
         response = self.request(user=user)
         assert response.status_code == 200
         assert response.content == self.render_content(
-            projects=Project.objects.all(),
-            show_members=True)
+            projects=Project.objects.all())
 
     def test_get_org_with_new_org(self):
         new_org = OrganizationFactory.create()
@@ -224,7 +223,6 @@ class OrganizationDashboardTest(ViewTestCase, UserTestCase, TestCase):
             is_superuser=True,
             is_administrator=True,
             add_allowed=True,
-            show_members=True,
             projects=self.all_projects)
 
     def test_get_org_with_org_admin(self):
@@ -241,7 +239,6 @@ class OrganizationDashboardTest(ViewTestCase, UserTestCase, TestCase):
             is_superuser=False,
             is_administrator=True,
             add_allowed=True,
-            show_members=True,
             projects=self.all_projects)
 
     def test_get_archived_org_with_unauthorized_user(self):
@@ -276,7 +273,6 @@ class OrganizationDashboardTest(ViewTestCase, UserTestCase, TestCase):
             is_superuser=False,
             is_administrator=True,
             add_allowed=True,
-            show_members=True,
             projects=self.all_projects)
 
 
--- cadasta/organization/views/default.py
+++ cadasta/organization/views/default.py
@@ -73,7 +73,6 @@ class OrganizationDashboard(PermissionRequiredMixin,
         context = self.get_context_data()
         if self.is_superuser:
             projects = self.object.all_projects()
-            show_members = True
         else:
             projects = self.object.public_projects()
             if hasattr(self.request.user, 'organizations'):
@@ -85,15 +84,7 @@ class OrganizationDashboard(PermissionRequiredMixin,
                         else:
                             projects = self.object.all_projects().filter(
                                 archived=False)
-
-            if not self.request.user.is_anonymous():
-                show_members = OrganizationRole.objects.filter(
-                    user=self.request.user,
-                    organization=context['organization']).exists()
-            else:
-                show_members = False
         context['projects'] = projects
-        context['show_members'] = show_members
         return super(generic.DetailView, self).render_to_response(context)
 
 
