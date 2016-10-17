2bfb115178c59bb89bd25302ef183d814454cc6d
Lindsey Jacks <linzjax@gmail.com>
2016-10-17 16:52:56 -0400

    Whitelisted all audio types except vnd.*

--- cadasta/resources/tests/test_validators.py
+++ cadasta/resources/tests/test_validators.py
@@ -19,3 +19,19 @@ class ValidateFileTypeTest(TestCase):
             validate_file_type('application/x-empty')
         assert e.value.message == (
             'Files of type application/x-empty are not accepted.')
+
+    def test_valid_audio_file(self):
+        try:
+            validate_file_type('audio/amr')
+        except ValidationError:
+            pytest.fail('ValidationError raised unexpectedly')
+        try:
+            validate_file_type('audio/m4a')
+        except ValidationError:
+            pytest.fail('ValidationError raised unexpectedly')
+
+    def test_invalid_audio_file(self):
+        with pytest.raises(ValidationError) as e:
+            validate_file_type('audio/vnd.dts')
+        assert e.value.message == (
+            'Files of type audio/vnd.dts are not accepted.')
