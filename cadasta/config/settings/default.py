7bf02892306aa4fbe5ba1a493edeb1f93fba327e
amplifi <amplifi@users.noreply.github.com>
2016-10-19 18:18:38 +1000

    One comma

--- cadasta/config/settings/default.py
+++ cadasta/config/settings/default.py
@@ -84,7 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'audit_log.middleware.UserLoggingMiddleware',
-    'simple_history.middleware.HistoryRequestMiddleware'
+    'simple_history.middleware.HistoryRequestMiddleware',
     'opbeat.contrib.django.middleware.Opbeat404CatchMiddleware',
 )
 
