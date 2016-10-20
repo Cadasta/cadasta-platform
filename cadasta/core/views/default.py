4f19bb9100b6136b247fd8f2c373dcbbb55e7857
Ian Ross <iross@cadasta.org>
2016-10-20 14:49:19 +0200

    Fix rebase mistake

--- cadasta/core/views/default.py
+++ cadasta/core/views/default.py
@@ -45,10 +45,6 @@ class Dashboard(TemplateView):
                                 access='public',
                                 extent__isnull=False,
                                 archived=False).select_related('organization'))
-            projects.extend(Project.objects.filter(
-                access='public',
-                extent__isnull=False,
-                archived=False).select_related('organization'))
         context = self.get_context_data(projects=projects)
         return super(TemplateView, self).render_to_response(context)
 
