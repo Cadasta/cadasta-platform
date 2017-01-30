02d0a214971e8df61492655b378d007e9f2bbba0
Lindsey Jacks <linzjax@gmail.com>
2017-01-30 10:14:27 -0500

    implemented tile->lat/long converter
    adding L.Deflate back
    
    added L.TileLayer.GeoJSON and updated to be compatable with Leaflet 1.0
    
    Currently only loaded spatial resources. Should update to drop SUs when out of view

--- cadasta/core/static/js/smap/index.js
+++ cadasta/core/static/js/smap/index.js
@@ -1,5 +1,5 @@
 $(window).load(function () {
-  var js_files = ['map.js']
+  var js_files = ['L.Map.Deflate4.js', 'L.TileLayer.GeoJSON.js', 'map.js']
   var body = $('body')
   for (i in js_files) {
     body.append($('<script src="/static/js/smap/' + js_files[i] + '"></script>'));
