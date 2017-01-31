72e9982fb24a6bccf5bd5efef8cde5fd349957b8
bohare <bohare@cadasta.org>
2017-01-31 12:27:17 +0000

    Fix #813: Import conditional attributes

--- cadasta/organization/tests/test_importers.py
+++ cadasta/organization/tests/test_importers.py
@@ -2,10 +2,11 @@ import pytest
 
 import pandas as pd
 from core.tests.utils.cases import FileStorageTestCase, UserTestCase
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.gis.geos import LineString, Point, Polygon
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from jsonattrs.models import Attribute, Schema
+from jsonattrs.models import Attribute, AttributeType, Schema
 from party.models import Party, TenureRelationship
 from questionnaires.models import Questionnaire
 from resources.tests.utils import clear_temp  # noqa
@@ -27,7 +28,7 @@ class BaseImporterTest(UserTestCase, TestCase):
         project = ProjectFactory.create()
         importer = Importer(project)
         assert len(importer.get_schema_attrs().keys()) == 5
-        assert importer.get_schema_attrs()['spatial.spatialunit'] == []
+        assert importer.get_schema_attrs()['spatial.spatialunit'] == {}
 
     def test_import_data_not_implemented(self):
         project = ProjectFactory.create()
@@ -40,6 +41,22 @@ class BaseImporterTest(UserTestCase, TestCase):
         importer = Importer(project)
         with pytest.raises(NotImplementedError):
             importer.get_headers()
+
+    def test_cast_to_type(self):
+        project = ProjectFactory.create()
+        importer = Importer(project)
+        val = importer._cast_to_type('1.0', 'integer')
+        assert type(val) is int
+        assert val == 1
+        val = importer._cast_to_type('not an integer', 'integer')
+        assert type(val) is int
+        assert val == 0
+        val = importer._cast_to_type('1', 'decimal')
+        assert type(val) is float
+        assert val == 1.0
+        val = importer._cast_to_type('not a decimal', 'decimal')
+        assert type(val) is float
+        assert val == 0.0
 
 
 class ImportValidatorTest(TestCase):
@@ -220,6 +237,9 @@ class CSVImportTest(UserTestCase, FileStorageTestCase, TestCase):
             'party::j_l', 'party::name_father_hus', 'party::village_name',
             'party::mobile_no', 'party::occupation_hh', 'party::class_hh'
         ]
+        self.party_gr_attributes = [
+            'party::test_group_attr'
+        ]
         self.location_attributes = [
             'spatialunit::deed_of_land', 'spatialunit::amount_othersland',
             'spatialunit::land_calculation', 'spatialunit::how_aquire_landwh',
@@ -247,10 +267,16 @@ class CSVImportTest(UserTestCase, FileStorageTestCase, TestCase):
         importer = csv.CSVImporter(
             project=self.project, path=self.path + self.valid_csv)
         attrs = importer.get_schema_attrs()
-        su_attrs = attrs['spatial.spatialunit']
-        pty_attrs = attrs['party.party']
+        su_attrs = attrs['spatial.spatialunit']['DEFAULT']
+        pty_attrs_in = attrs['party.party']['IN']
+        pty_attrs_gr = attrs['party.party']['GR']
+        pty_attrs_co = attrs['party.party']['CO']
+        tenure_attrs = attrs['party.tenurerelationship']['DEFAULT']
         assert len(su_attrs) == 29
-        assert len(pty_attrs) == 11
+        assert len(pty_attrs_in) == 11
+        assert len(pty_attrs_gr) == 11
+        assert len(pty_attrs_co) == 11
+        assert len(tenure_attrs) == 2
 
     def test_get_attribute_map(self):
         importer = csv.CSVImporter(
@@ -262,9 +288,64 @@ class CSVImportTest(UserTestCase, FileStorageTestCase, TestCase):
         assert len(attr_map.keys()) == 3
         assert len(extra_attrs) == 11
         assert len(extra_headers) == 10
-        assert attr_map['party']['class_hh'][1] == 'party.party'
-        assert isinstance(attr_map['spatialunit']
-                          ['others_conflict'][0], Attribute)
+
+        # assert correct length of conditional selectors
+        assert len(attr_map['party.party']) == 3
+        assert len(attr_map['party.party']['CO']) == 8
+        assert len(attr_map['party.party']['GR']) == 8
+        assert len(attr_map['party.party']['IN']) == 8
+
+        # assert correct length of default selectors
+        assert len(attr_map['spatial.spatialunit']) == 1
+        assert len(attr_map['spatial.spatialunit']['DEFAULT']) == 21
+        assert len(attr_map['party.tenurerelationship']['DEFAULT']) == 2
+
+        # assert correctness of attribute map contents
+        (attr, content_type, label) = attr_map['party.party'][
+            'CO']['name_mouza']
+        assert isinstance(attr, Attribute)
+        assert attr.name == 'name_mouza'
+        assert content_type == 'party.party'
+        assert label == 'Party'
+
+    def test_get_attribute_map_parties_only(self):
+        importer = csv.CSVImporter(
+            project=self.project, path=self.path + self.valid_csv)
+        entity_types = ['PT']
+        attr_map, extra_attrs, extra_headers = importer.get_attribute_map(
+            'csv', entity_types
+        )
+        assert len(attr_map.keys()) == 1
+        assert len(extra_attrs) == 3
+        assert len(extra_headers) == 10
+
+        # assert correct length of conditional selectors
+        assert len(attr_map['party.party']) == 3
+        assert len(attr_map['party.party']['CO']) == 8
+        assert len(attr_map['party.party']['GR']) == 8
+        assert len(attr_map['party.party']['IN']) == 8
+
+        # assert correctness of attribute map contents
+        (attr, content_type, label) = attr_map['party.party'][
+            'CO']['name_mouza']
+        assert isinstance(attr, Attribute)
+        assert attr.name == 'name_mouza'
+        assert content_type == 'party.party'
+        assert label == 'Party'
+
+    def test_get_flattened_attribute_map(self):
+        importer = csv.CSVImporter(
+            project=self.project, path=self.path + self.valid_csv)
+        entity_types = ['PT', 'SU']
+        attr_map, extra_attrs, extra_headers = importer.get_attribute_map(
+            'csv', entity_types, flatten=True
+        )
+        party_attrs = attr_map['party']
+        su_attrs = attr_map['spatialunit']
+        tenure_attrs = attr_map['tenurerelationship']
+        assert len(party_attrs) == 8
+        assert len(su_attrs) == 21
+        assert len(tenure_attrs) == 2
 
     def test_import_data(self):
         importer = csv.CSVImporter(
@@ -299,7 +380,7 @@ class CSVImportTest(UserTestCase, FileStorageTestCase, TestCase):
         assert su.attributes[
             'others_conflict'] == ('কিছু বেখলে অাছে জোর করে দখল করে ভোগ করে '
                                    'অন্য জমির মালক।')
-        assert su.attributes['female_member'] == '4'
+        assert su.attributes['female_member'] == 4
         assert su.attributes['location_problems'] == [
             'conflict', 'risk_of_eviction'
         ]
@@ -376,7 +457,7 @@ class CSVImportTest(UserTestCase, FileStorageTestCase, TestCase):
         assert su.attributes[
             'others_conflict'] == ('কিছু বেখলে অাছে জোর করে দখল করে ভোগ করে '
                                    'অন্য জমির মালক।')
-        assert su.attributes['female_member'] == '4'
+        assert su.attributes['female_member'] == 4
         assert su.attributes['location_problems'] == [
             'conflict', 'risk_of_eviction'
         ]
@@ -545,7 +626,7 @@ class XLSImportTest(UserTestCase, FileStorageTestCase, TestCase):
         assert su.attributes[
             'others_conflict'] == ('কিছু বেখলে অাছে জোর করে দখল করে ভোগ করে '
                                    'অন্য জমির মালক।')
-        assert su.attributes['female_member'] == '4'
+        assert su.attributes['female_member'] == 4
         assert su.attributes['location_problems'] == [
             'conflict', 'risk_of_eviction'
         ]
@@ -598,7 +679,7 @@ class XLSImportTest(UserTestCase, FileStorageTestCase, TestCase):
         assert su.attributes[
             'others_conflict'] == ('কিছু বেখলে অাছে জোর করে দখল করে ভোগ করে '
                                    'অন্য জমির মালক।')
-        assert su.attributes['female_member'] == '4'
+        assert su.attributes['female_member'] == 4
         assert su.attributes['location_problems'] == [
             'conflict', 'risk_of_eviction'
         ]
@@ -693,3 +774,212 @@ class XLSImportTest(UserTestCase, FileStorageTestCase, TestCase):
         assert str(e.value) == (
             'Error importing file: Unsupported import format.'
         )
+
+
+class ImportConditionalAttributesTest(UserTestCase, FileStorageTestCase,
+                                      TestCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.conditionals_csv = (
+            '/organization/tests/files/test_conditionals.csv')
+        self.conditionals_xls = (
+            '/organization/tests/files/test_conditionals.xlsx')
+        self.project = ProjectFactory.create(current_questionnaire='123abc')
+        self.spatial_content_type = ContentType.objects.get(
+            app_label='spatial', model='spatialunit'
+        )
+        sp_schema = Schema.objects.create(
+            content_type=self.spatial_content_type,
+            selectors=(
+                self.project.organization.id, self.project.id, '123abc', ))
+        attr_type = AttributeType.objects.get(name='text')
+        Attribute.objects.create(
+            schema=sp_schema,
+            name='su_attr', long_name='Test field',
+            attr_type=attr_type, index=0,
+            required=False, omit=False
+        )
+
+        self.party_content_type = ContentType.objects.get(
+            app_label='party', model='party'
+        )
+        pt_schema = Schema.objects.create(
+            content_type=self.party_content_type,
+            selectors=(
+                self.project.organization.id, self.project.id, '123abc', ))
+        pt_schema_in = Schema.objects.create(
+            content_type=self.party_content_type,
+            selectors=(
+                self.project.organization.id, self.project.id, '123abc', 'IN'))
+        pt_schema_gr = Schema.objects.create(
+            content_type=self.party_content_type,
+            selectors=(
+                self.project.organization.id, self.project.id, '123abc', 'GR'))
+        attr_type_txt = AttributeType.objects.get(name='text')
+        attr_type_int = AttributeType.objects.get(name='integer')
+        attr_type_dec = AttributeType.objects.get(name='decimal')
+
+        Attribute.objects.create(
+            schema=pt_schema,
+            name='default_attr', long_name='Test field',
+            attr_type=attr_type_txt, index=0,
+            required=False, omit=False
+        )
+        Attribute.objects.create(
+            schema=pt_schema,
+            name='default_int_attr', long_name='Test integer field',
+            attr_type=attr_type_int, index=1,
+            required=False, omit=False
+        )
+        Attribute.objects.create(
+            schema=pt_schema_in,
+            name='party_in', long_name='Test IN field',
+            attr_type=attr_type_txt, index=0,
+            required=False, omit=False
+        )
+        Attribute.objects.create(
+            schema=pt_schema_gr,
+            name='party_gr', long_name='Test GR field',
+            attr_type=attr_type_txt, index=0,
+            required=False, omit=False
+        )
+        Attribute.objects.create(
+            schema=pt_schema_gr,
+            name='party_gr_dec', long_name='Test GR dec field',
+            attr_type=attr_type_dec, index=1,
+            required=False, omit=False
+        )
+
+        self.party_default_attributes = [
+            'party::default_attr', 'party::default_int_attr'
+        ]
+        self.party_in_attributes = [
+            'party::party_in'
+        ]
+        self.party_gr_attributes = [
+            'party::party_gr', 'party::party_gr_dec'
+        ]
+        self.party_attributes = (
+            self.party_default_attributes + self.party_in_attributes +
+            self.party_gr_attributes
+        )
+
+        self.location_attributes = ['spatialunit::su_attr']
+        self.attributes = (
+            self.location_attributes + self.party_attributes
+        )
+
+    def test_get_flattened_attribute_map(self):
+        importer = csv.CSVImporter(
+            project=self.project, path=self.path + self.conditionals_csv)
+        entity_types = ['PT', 'SU']
+        attr_map, extra_attrs, extra_headers = importer.get_attribute_map(
+            'csv', entity_types, flatten=True
+        )
+        party_attrs = attr_map['party']
+        assert len(party_attrs) == 5
+
+    def test_import_csv(self):
+        importer = csv.CSVImporter(
+            project=self.project, path=self.path + self.conditionals_csv)
+        config = {
+            'file': self.path + self.conditionals_csv,
+            'entity_types': ['PT', 'SU'],
+            'party_name_field': 'party_name',
+            'party_type_field': 'party_type',
+            'location_type_field': 'location_type',
+            'geometry_field': 'location_geometry',
+            'attributes': self.attributes,
+            'project': self.project
+        }
+        importer.import_data(config)
+        assert Party.objects.all().count() == 2
+        assert SpatialUnit.objects.all().count() == 2
+        assert TenureRelationship.objects.all().count() == 2
+        for su in SpatialUnit.objects.filter(project_id=self.project.pk).all():
+            if su.geometry is not None:
+                assert type(su.geometry) is Point
+
+        # test spatial unit attribute creation
+        sus = SpatialUnit.objects.filter(
+            attributes__contains={'su_attr': 'some value'})
+        assert len(sus) == 1
+        su = sus[0]
+        assert su.type == 'PA'
+        assert len(su.attributes) == 1
+
+        # test party attribute creation
+        party_in = Party.objects.filter(type='IN').first()
+        assert party_in.name == 'Test Party'
+        pty_attrs = party_in.attributes
+        assert len(pty_attrs) == 3
+        assert pty_attrs['default_attr'] == 'test default attr in'
+        assert pty_attrs['default_int_attr'] == 10
+        assert pty_attrs['party_in'] == 'some individual value'
+
+        party_gr = Party.objects.filter(type='GR').first()
+        assert party_gr.name == 'Another Test Party'
+        pty_attrs = party_gr.attributes
+        assert len(pty_attrs) == 4
+        assert pty_attrs['default_attr'] == 'test default attr gr'
+        assert pty_attrs['default_int_attr'] == 20
+        assert pty_attrs['party_gr'] == 'some group value'
+        assert pty_attrs['party_gr_dec'] == 2.00
+
+        # test tenure relationship creation
+        tenure_relationships = TenureRelationship.objects.all()
+        assert len(tenure_relationships) == 2
+
+    def test_import_xlsx(self):
+        importer = xls.XLSImporter(
+            project=self.project, path=self.path + self.conditionals_xls)
+        config = {
+            'file': self.path + self.conditionals_xls,
+            'type': 'xls',
+            'entity_types': ['SU', 'PT'],
+            'party_name_field': 'name',
+            'party_type_field': 'type',
+            'location_type_field': 'type',
+            'geometry_field': 'geometry.ewkt',
+            'attributes': self.attributes,
+            'project': self.project
+        }
+        importer.import_data(config)
+        assert Party.objects.all().count() == 2
+        assert SpatialUnit.objects.all().count() == 2
+        assert TenureRelationship.objects.all().count() == 2
+        for su in SpatialUnit.objects.filter(project_id=self.project.pk).all():
+            if su.geometry is not None:
+                assert type(su.geometry) is Point
+
+        # test spatial unit attribute creation
+        sus = SpatialUnit.objects.filter(
+            attributes__contains={'su_attr': 'some value'})
+        assert len(sus) == 1
+        su = sus[0]
+        assert su.type == 'PA'
+        assert len(su.attributes) == 1
+
+        # test party attribute creation
+        party_in = Party.objects.filter(type='IN').first()
+        assert party_in.name == 'Another Test Party'
+        pty_attrs = party_in.attributes
+        assert len(pty_attrs) == 3
+        assert pty_attrs['default_attr'] == 'test default attr in'
+        assert pty_attrs['default_int_attr'] == 10
+        assert pty_attrs['party_in'] == 'some individual value'
+
+        party_gr = Party.objects.filter(type='GR').first()
+        assert party_gr.name == 'Test Party'
+        pty_attrs = party_gr.attributes
+        assert len(pty_attrs) == 4
+        assert pty_attrs['default_attr'] == 'test default attr gr'
+        assert pty_attrs['default_int_attr'] == 20
+        assert pty_attrs['party_gr'] == 'some group value'
+        assert pty_attrs['party_gr_dec'] == 2.0
+
+        # test tenure relationship creation
+        tenure_relationships = TenureRelationship.objects.all()
+        assert len(tenure_relationships) == 2
