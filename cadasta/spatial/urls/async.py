ea63da2fb9a1d1aaca829738a078e297a696cce4
Lindsey Jacks <linzjax@gmail.com>
2017-02-15 10:38:11 -0500

    Implement Vector Tiling for SMAP (#1099)
    * implemented tile->lat/long converter
    
    adding L.Deflate back
    
    added L.TileLayer.GeoJSON and updated to be compatable with Leaflet 1.0
    
    Currently only loaded spatial resources. Should update to drop SUs when out of view
    
    * Lazy tile loading

--- cadasta/spatial/urls/async.py
+++ cadasta/spatial/urls/async.py
@@ -8,6 +8,10 @@ urls = [
         r'^$',
         async.SpatialUnitList.as_view(),
         name='list'),
+    url(
+        r'^tiled/(?P<z>[0-9]+)/(?P<x>[0-9]+)/(?P<y>[0-9]+)/$',
+        async.SpatialUnitTiles.as_view(),
+        name='tiled'),
 ]
 
 
@@ -16,4 +20,8 @@ urlpatterns = [
         r'^organizations/(?P<organization>[-\w]+)/projects/'
         '(?P<project>[-\w]+)/spatial/',
         include(urls)),
+    # url(urls.tilepath(r'^organizations/(?P<organization>[-\w]+)/projects/'
+    #     '(?P<project>[-\w]+)/spatial/'),
+    #     async.SpatialUnitList.as_view(),
+    #     name='location-tiles'),
 ]
