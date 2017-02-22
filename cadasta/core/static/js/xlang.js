6ba66de7a2c24f0b4bbcfe034893199929a478fe
Oliver Roick <oliver.roick@gmail.com>
2017-02-22 10:32:07 +0100

    Select default lang for a form; store selected lang per project

--- cadasta/core/static/js/xlang.js
+++ cadasta/core/static/js/xlang.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  const storage_key = 'form_lang::' + $('#form-langs-select').attr('data-project-slug')
   function setLabels(lang) {
     const labels = $('label, option, td, a, div');
 
@@ -13,13 +14,18 @@ $(document).ready(function () {
 
   $('#form-langs-select').change(function() {
     const new_lang = $(this).val();
-    localStorage.setItem("form_lang", new_lang);
+    localStorage.setItem(storage_key, new_lang);
     setLabels(new_lang);
   });
 
-  const form_lang = localStorage.getItem("form_lang");
+  const form_lang = localStorage.getItem(storage_key);
   if (form_lang && $('#form-langs-select option[value="' + form_lang + '"]').length) {
     $('#form-langs-select').val(form_lang);
     setLabels(form_lang);
+  } else {
+    const default_lang = $('#form-langs-select').attr('data-default-lang');
+    $('#form-langs-select').val(default_lang);
+    localStorage.setItem(storage_key, default_lang);
+    setLabels(default_lang);
   }
 });
