58de45c547dd44f10c780b26d41f96ed4f1115a9
Laura Barluzzi <laura@koalacoder.com>
2017-06-02 16:36:29 -0700

    add custom middleware to set user language on client requests

---
+++ cadasta/config/middleware/user_language_middleware.py
@@ -0,0 +1,15 @@
+from django.utils import translation
+
+class UserLanguageMiddleware(object):
+    def process_request(self, request):
+        if not request.user.is_authenticated():
+            return None
+
+        user_language = request.user.language
+        current_language = translation.get_language()
+        if user_language == current_language:
+            return None
+
+        translation.activate(user_language)
+        request.session[translation.LANGUAGE_SESSION_KEY] = user_language
+        return None
