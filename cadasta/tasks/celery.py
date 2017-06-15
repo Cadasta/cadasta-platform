3bc45f688245d3014acdbcbd9b0d9878335b50f8
Anthony Lukach <alukach@cadasta.org>
2017-06-14 22:52:57 -0600

    Slightly working

--- cadasta/tasks/celery.py
+++ cadasta/tasks/celery.py
@@ -1,45 +1,6 @@
 from celery import Celery
 
 app = Celery()
-# app.conf.worker_consumer = 'tasks.consumers:ResultConsumer'
+app.conf.worker_consumer = 'tasks.consumers:ResultConsumer'
 # app.conf.result_backend = 'tasks.backends:ResultQueueRPC'
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# from celery import bootsteps
-
-# class InfoStep(bootsteps.Step):
-
-#     def __init__(self, parent, **kwargs):
-#         # here we can prepare the Worker/Consumer object
-#         # in any way we want, set attribute defaults, and so on.
-#         print('{0!r} is in init'.format(parent))
-
-#     def start(self, parent):
-#         # our step is started together with all other Worker/Consumer
-#         # bootsteps.
-#         print('{0!r} is starting'.format(parent))
-
-#     def stop(self, parent):
-#         # the Consumer calls stop every time the consumer is
-#         # restarted (i.e., connection is lost) and also at shutdown.
-#         # The Worker will call stop at shutdown only.
-#         print('{0!r} is stopping'.format(parent))
-
-#     def shutdown(self, parent):
-#         # shutdown is called by the Consumer at shutdown, it's not
-#         # called by Worker.
-#         print('{0!r} is shutting down'.format(parent))
-
-
-# class WorkerStep(bootsteps.Step):
-#     def start(self, worker):
-#         print("FOOBAR")
-#         from celery.contrib import rdb
-#         rdb.set_trace()
-#         for task in [t for t in self.app.tasks]:
-#             if not task.startswith('result_consumer'):
-#                 self.app.tasks.unregister(task)
-#                 print("Rm {!r}".format(task))
-
-# app.steps['worker'].add(WorkerStep)
-# app.steps['consumer'].add(InfoStep)
--- cadasta/tasks/consumers.py
+++ cadasta/tasks/consumers.py
@@ -12,15 +12,15 @@ from kombu import Queue, Consumer as KombuConsumer
 from vine import promise
 
 from .celery import app
+from .models import BackgroundTask
 
 
 logger = logging.getLogger(__name__)
 
 
-# @app.task(name='result_consumer.process_task')
-def process_task(message):
-    from .models import BackgroundTask
+def process_task(message, *args, **kwargs):
     """ Add scheduled tasks to database """
+    from celery.contrib import rdb; rdb.set_trace()
     try:
         args, kwargs, options = message.decode()
         task_id = message.headers['id']
@@ -59,14 +59,13 @@ def process_task(message):
         raise InvalidTaskError("Failed to parse task.")
 
 
-# @app.task(name='result_consumer.process_result')
-def process_result(message):
+def process_result(message, *args, **kwargs):
     """ Handle result message """
     try:
+        # from celery.contrib import rdb; rdb.set_trace()
         result = message.payload
         logger.debug('Received message: {0!r}'.format(result))
         task_qs = BackgroundTask.objects.filter(id=result['task_id'])
-        # from celery.contrib import rdb; rdb.set_trace()
         assert task_qs.exists()
 
         status = result.get('status')
@@ -89,149 +88,7 @@ def process_result(message):
         raise InvalidTaskError("Failed to process task result.")
 
 
-# class ResultConsumerStep(bootsteps.StartStopStep):
-# # class ResultConsumerStep(bootsteps.ConsumerStep):
-#     requires = ('celery.worker.consumer:Tasks', )
-#     def start(self, parent):
-#         valid_queues = [
-#             settings.CELERY_DEFAULT_QUEUE,
-#             settings.CELERY_RESULT_QUEUE
-#         ]
-#         # from celery.contrib import rdb
-#         # rdb.set_trace()
-#         for name, q in parent.app.amqp.queues.items():
-#             if name not in valid_queues:
-#                 parent.cancel_task_queue(name)
-
-#         to_rm = []
-#         for task in parent.task_buckets:
-#             if not task.startswith('result_consumer'):
-#                 to_rm.append(task)
-#         for task in to_rm:
-#             parent.task_buckets.pop(task)
-
-#         # parent.cancel_task_queue(app.conf.CELERY_DEFAULT_QUEUE)
-#         # parent.add_task_queue('{}.{}'.format(some_id, self.my_queue))
-
-
-# class ResultConsumerStep(bootsteps.ConsumerStep):
-#     """
-#     Reads off the Result queue, inserting messages into DB.
-
-#     NOTE: This only works if you run a celery worker that is NOT looking
-#     at the Result queue. Ex. "celery -A config worker"
-#     """
-
-#     # def __init__(self, *args, **kwargs):
-#     #     if settings.CELERY_RESULT_QUEUE in app.app_or_default().amqp.queues:
-#     #         msg = (
-#     #             "Error: Please don't specify the result queue (\"{}\") as a "
-#     #             "queue to be consumed by the worker. Result messages aren't "
-#     #             "encoded in the same format as Task messages. Bad things will "
-#     #             "happen."
-#     #         )
-#     #         raise ValueError(msg.format(settings.CELERY_RESULT_QUEUE))
-#     #     super(ResultConsumer, self).__init__(*args, **kwargs)
-
-#     @staticmethod
-#     def _detect_msg_type(message):
-#         if 'task' in message.headers:
-#             return 'task'
-#         if 'result' in message.payload:
-#             return 'result'
-#         raise TypeError("Cannot detect message type")
-
-#     def get_consumers(self, channel):
-#         return [KombuConsumer(channel,
-#                          queues=[Queue('result_queue.fifo')],
-#                          callbacks=[self.handle_message],
-#                          accept=['json'])]
-
-#     def handle_message(self, body, message):
-#         # from celery.contrib import rdb
-#         # rdb.set_trace()
-
-#         try:
-#             handler = {
-#                 'task': process_task,
-#                 'result': process_result
-#             }[self._detect_msg_type(message)]
-#             handler(message)
-
-#             # logger.debug('Received message: {0!r}'.format(body))
-#             # task_qs = BackgroundTask.objects.filter(id=body['task_id'])
-#             # assert task_qs.exists()
-
-#             # status = body.get('status')
-#             # if status:
-#             #     task_qs.update(status=status)
-
-#             # result = body['result']
-#             # if status in BackgroundTask.DONE_STATES:
-#             #     task_qs.update(output=result)
-#             # else:
-#             #     assert isinstance(result, dict), (
-#             #         "Malformed result data, expecting a dict"
-#             #     )
-#             #     log = result.get('log')
-#             #     if log:
-#             #         task_qs.update(log=CombinedExpression(
-#             #             F('log'), '||', Value([log])
-#             #         ))
-#         except:
-#             logger.exception("Failed to process task")
-#         finally:
-#             try:
-#                 message.ack()
-#             except:
-#                 logger.exception("Failed to acknowledge message")
-
-class WorkerStep(bootsteps.Step):
-    def start(self, worker):
-        from celery.contrib import rdb
-        rdb.set_trace()
-        for task in [t for t in self.app.tasks]:
-            if not task.startswith('result_consumer'):
-                self.app.tasks.unregister(task)
-                print("Rm {!r}".format(task))
-
-
 class ResultConsumer(Consumer):
-    def __init__(self, *args, **kwargs):
-        super(ResultConsumer, self).__init__(*args, **kwargs)
-        # from celery.contrib import rdb
-        # rdb.set_trace()
-        # print(type(self.app.tasks))
-        for task in [t for t in self.app.tasks]:
-            if not task.startswith('result_consumer'):
-                self.app.tasks.unregister(task)
-        # self.app.tasks = {
-        #     k: v for k, v in self.app.tasks.items()
-        #     if k.startswith('result_consumer')}
-
-        # valid_queues = [
-        #     settings.CELERY_DEFAULT_QUEUE,
-        #     settings.CELERY_RESULT_QUEUE
-        # ]
-        # # from celery.contrib import rdb
-        # # rdb.set_trace()
-        # for name, q in self.app.amqp.queues.items():
-        #     if name not in valid_queues:
-        #         self.cancel_task_queue(name)
-        # from celery.contrib import rdb
-        # rdb.set_trace()
-        # # self.app.backend = DisabledBackend(self.app)
-
-    def update_strategies(self):
-        self.strategies = {
-            'task': process_task,
-            'result': process_result,
-        }
-        loader = self.app.loader
-        for name, task in self.strategies.items():
-            self.strategies[name] = task.start_strategy(self.app, self)
-            task.__trace__ = build_tracer(name, task, loader, self.hostname,
-                                          app=self.app)
 
     @staticmethod
     def _detect_msg_type(message):
@@ -242,7 +99,10 @@ class ResultConsumer(Consumer):
         raise TypeError("Cannot detect message type")
 
     def create_task_handler(self, promise=promise):
-        strategies = self.strategies
+        strategies = {
+            'task': process_task,
+            'result': process_result,
+        }
         on_unknown_message = self.on_unknown_message
         on_unknown_task = self.on_unknown_task
         on_invalid_task = self.on_invalid_task
@@ -250,15 +110,17 @@ class ResultConsumer(Consumer):
         call_soon = self.call_soon
 
         def on_task_received(message):
-            # from celery.contrib import rdb; rdb.set_trace()
+            print("RECEIVED TASKS")
 
             try:
                 type_ = self._detect_msg_type(message)
             except TypeError as exc:
                 return on_unknown_message(None, message, exc)
             try:
-                strategy = strategies[type_]
+                handler = app.task(strategies[type_])
+                strategy = handler.start_strategy(app, self)
             except KeyError as exc:
+                from celery.contrib import rdb; rdb.set_trace()
                 return on_unknown_task(None, message, exc)
             try:
                 strategy(
