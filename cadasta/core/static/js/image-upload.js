ff76145210bb9e70c495dc780fb5839f1f02a693
Laura Barluzzi <laura@koalacoder.com>
2017-07-04 15:42:39 -0700

    display range for zoom only after uploading new image

--- cadasta/core/static/js/image-upload.js
+++ cadasta/core/static/js/image-upload.js
@@ -1,5 +1,11 @@
 $(function() {
+    $("input[name='zoom']").hide();
     $('.image-editor').cropit();
+
+    $("input[name='new-image']").change(function() {
+    $("input[name='zoom']").show();
+    });
+
     $('form').submit(function() {
     var imageData = $('.image-editor').cropit('export');
     $('.hidden-image-data').val(imageData);
