940719e1eccfe36acdce0dd15066f2a035dd4001
Laura Barluzzi <laura@koalacoder.com>
2017-07-06 19:52:25 -0700

    simplify cropping flow

--- cadasta/core/static/js/image-upload.js
+++ cadasta/core/static/js/image-upload.js
@@ -2,66 +2,30 @@ $(function() {
     var URL_DEFAULT_AVATAR = '/static/img/avatar_sm.jpg';
     var URL_USER_AVATAR = $('#user-avatar').attr('src');
     var avatarChanged = false;
-    var avatarReady = false;
-    var hideZoomRange = false;
-
-    function setVisibilityZoomRange(hideZoomRange) {
-        if (hideZoomRange) {
-            $("#zoom").addClass("hidden");
-        } else {
-            $("#zoom").removeClass("hidden");
-        }
-    }
-
-    function fetchBase64ForImage(imageUrl, imageWidth, imageHeight, callback) {
-        var image = new Image();
-        image.onload = function() {
-            var canvas = document.createElement("canvas");
-            canvas.width = imageWidth;
-            canvas.height = imageHeight;
-            var ctx = canvas.getContext("2d");
-            ctx.drawImage(this, 0, 0);
-            var dataURL = canvas.toDataURL("image/png");
-            callback(dataURL);
-        };
-        image.src = imageUrl;
-    }
-
-    function setProfilePictureToDefaultAvatar() {
-        avatarReady = false;
-        avatarChanged = true;
-        fetchBase64ForImage(URL_DEFAULT_AVATAR, 200, 200, function(imageData) {
-            $('.hidden-image-data').val(imageData);
-            $('.image-editor').cropit('imageSrc', URL_DEFAULT_AVATAR);
-            avatarReady = true;
-            hideZoomRange = true;
-        });
-    }
-
-    function setProfilePictureToUploadedAvatar() {
-        avatarReady = false;
-        avatarChanged = true;
-        $("#user-avatar").hide();
-        var imageData = $('.image-editor').cropit('export');
-        $('.hidden-image-data').val(imageData);
-        setVisibilityZoomRange(hideZoomRange);
-        avatarReady = true;
-        hideZoomRange = false;
-    }
 
     $('.image-editor').cropit({
-        onImageLoaded: setProfilePictureToUploadedAvatar
+        onFileChange: function() {
+            avatarChanged = true;
+            $("#user-avatar").hide();
+            $("#zoom").removeClass("hidden");
+        }
     });
 
     $("#delete").click(function() {
-        setProfilePictureToDefaultAvatar();
+        avatarChanged = true;
+        $("#zoom").addClass("hidden");
+        $('.image-editor').cropit('imageSrc', URL_DEFAULT_AVATAR);
     });
 
     $("#undo").click(function() {
+        avatarChanged = false;
         $('.image-editor').cropit('imageSrc', URL_USER_AVATAR);
     });
 
     $('form').submit(function() {
-        return !avatarChanged || avatarReady;
+        if (avatarChanged) {
+            var imageData = $('.image-editor').cropit('export');
+            $('.hidden-image-data').val(imageData);
+        }
     });
 });
