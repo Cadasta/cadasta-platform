45a0f12dbbaa3e9e5ba1beca828d21bdc6681d76
Anthony Lukach <alukach@cadasta.org>
2017-09-13 22:01:04 -0600

    Limit length of select list for browsable api

--- cadasta/config/settings/default.py
+++ cadasta/config/settings/default.py
@@ -106,7 +106,8 @@ REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'core.views.api.exception_handler',
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'HTML_SELECT_CUTOFF': 100,
 }
 
 SITE_NAME = 'Cadasta'
