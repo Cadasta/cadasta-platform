10da7262c89314e9dcac99c1ff8e540970daeb4c
Anthony Lukach <anthonylukach@gmail.com>
2017-09-20 08:44:47 -0600

    Support Django Debug Toolbar for dev setups (#1803)

--- cadasta/config/urls/dev.py
+++ cadasta/config/urls/dev.py
@@ -8,3 +8,9 @@ urlpatterns = [
     url(r'', include('buckets.test.urls')),
     url(r'', include('search.mock_es.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+try:
+    import debug_toolbar
+    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
+except ImportError:
+    pass
