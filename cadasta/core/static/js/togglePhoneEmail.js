df73bc142ab629b0ede0b95cac0ae8f4df56d52d
Parthvi Vala <parthvi.vala@gmail.com>
2017-09-26 19:25:34 +0530

    fix bug
    minor changes

--- cadasta/core/static/js/togglePhoneEmail.js
+++ cadasta/core/static/js/togglePhoneEmail.js
@@ -4,15 +4,20 @@ $(document).ready(function() {
       $("#email-div").addClass("hidden");
       $('#id_phone').attr('data-parsley-required', 'true');
       $('#id_email').attr('data-parsley-required', 'false');
-      $('#id_email').parsley().reset();
       document.getElementById("id_email").value='';
+      $('#id_email').parsley().reset();
+      sessionStorage.setItem('phone',"true");
+      sessionStorage.removeItem('email', null);
+
   });
   $("#email-btn").click(function(){
       $("#email-div").removeClass("hidden");
       $("#phone-div").addClass("hidden");
       $('#id_phone').attr('data-parsley-required', 'false');
       $('#id_email').attr('data-parsley-required', 'true');
-      $('#id_phone').parsley().reset();
       document.getElementById("id_phone").value='';
+      $('#id_phone').parsley().reset();
+      sessionStorage.setItem('email',"true");
+      sessionStorage.removeItem('phone', null);
   });
 });
