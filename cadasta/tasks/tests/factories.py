1b2f863dbedb3fd46576fb67c37a6e16e8ee405d
Anthony Lukach <anthonylukach@gmail.com>
2017-10-11 09:53:15 -0600

    Async: Display export tasks on project dashboard (#1801)
    * Display export tasks on project dashboard

--- cadasta/tasks/tests/factories.py
+++ cadasta/tasks/tests/factories.py
@@ -8,12 +8,18 @@ from ..models import BackgroundTask, TaskResult
 from ..utils.fields import input_field_default
 
 
+def gen_id(length=24):
+    return str(uuid.uuid4())[:length]
+
+
 class BackgroundTaskFactory(ExtendedFactory):
 
     class Meta:
         model = BackgroundTask
 
-    id = str(uuid.uuid1())
+    id = factory.LazyFunction(gen_id)
+    task_id = factory.LazyFunction(gen_id)
+    root_id = factory.LazyAttribute(lambda o: o.task_id)
     type = 'foo.bar'
     input = factory.LazyFunction(input_field_default)
     creator = factory.SubFactory(UserFactory)
@@ -24,7 +30,6 @@ class TaskResultFactory(ExtendedFactory):
     class Meta:
         model = TaskResult
 
-    id = str(uuid.uuid1())
-    task_id = str(uuid.uuid1())
+    task = factory.SubFactory(BackgroundTaskFactory)
     result = 'complete'
     status = 'PENDING'
