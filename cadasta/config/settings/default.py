0bb94945af6344ed636cc1cdf65a18caf08ff86e
Oliver Roick <oliver.roick@gmail.com>
2017-11-15 15:45:45 +0100

    Add default TWILIO_PHONE

--- cadasta/config/settings/default.py
+++ cadasta/config/settings/default.py
@@ -581,3 +581,4 @@ SMS_GATEWAY = 'accounts.gateways.FakeGateway'
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE = '+123'
