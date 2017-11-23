5c92fcfc2ee5f7ea76430240a6a153bf4bf9a68d
Oliver Roick <oliver.roick@gmail.com>
2017-11-23 12:00:43 +0100

    Clean up gateway code

--- cadasta/accounts/gateways.py
+++ cadasta/accounts/gateways.py
@@ -8,13 +8,9 @@ fake_logger = logging.getLogger(name='accounts.FakeGateway')
 
 class TwilioGateway(object):
 
-    def __init__(self,
-                 account_sid=settings.TWILIO_ACCOUNT_SID,
-                 auth_token=settings.TWILIO_AUTH_TOKEN,
-                 from_phone_number_list=[]):
-
-        self.client = Client(account_sid, auth_token)
-        self.from_phone_number_list = from_phone_number_list
+    def __init__(self):
+        self.client = Client(settings.TWILIO_ACCOUNT_SID,
+                             settings.TWILIO_AUTH_TOKEN)
 
     def send_sms(self, to, body):
         try:
