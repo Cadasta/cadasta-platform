75edbdb83e0df3466417d9d263d1eae128b9a52c
Anthony Lukach <alukach@cadasta.org>
2017-12-20 17:10:07 -0700

    Annotate after count for faster queries

--- cadasta/spatial/views/async.py
+++ cadasta/spatial/views/async.py
@@ -9,14 +9,39 @@ from . import mixins
 from .. import serializers
 
 
-class Paginator(GeoJsonPagination):
+from django.core.paginator import Paginator
+
+
+class SpatialUnitDjangoPaginator(Paginator):
+
+    def page(self, number):
+        page = super().page(number)
+        # Here we are adding annotation after the page is selected. This helps
+        # us avoid the burden of the annotation's lookup clauses when we are
+        # doing the COUNT() operation needed to create the page.
+        annotation = {
+            # Prefetch the location type name
+            page.object_list.model._LOCATION_TYPE_KEY: Subquery(
+                QuestionOption.objects.filter(
+                    question__questionnaire_id=OuterRef(
+                        'project__current_questionnaire'),
+                    name=OuterRef('type')
+                ).values('label_xlat')
+            )
+        }
+        page.object_list = page.object_list.annotate(**annotation)
+        return page
+
+
+class SpatialUnitPaginator(GeoJsonPagination):
     page_size = 1000
+    django_paginator_class = SpatialUnitDjangoPaginator
 
 
 class SpatialUnitList(APIPermissionRequiredMixin,
                       mixins.SpatialQuerySetMixin,
                       generics.ListAPIView):
-    pagination_class = Paginator
+    pagination_class = SpatialUnitPaginator
     serializer_class = serializers.SpatialUnitGeoJsonSerializer
 
     def get_actions(self, request):
@@ -34,17 +59,7 @@ class SpatialUnitList(APIPermissionRequiredMixin,
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset(*args, **kwargs)
         queryset = queryset.exclude(id=self.request.GET.get('exclude'))
-        annotation = {
-            # Prefetch the location type name
-            queryset.model._LOCATION_TYPE_KEY: Subquery(
-                QuestionOption.objects.filter(
-                    question__questionnaire_id=OuterRef(
-                        'project__current_questionnaire'),
-                    name=OuterRef('type')
-                ).values('label_xlat')
-            )
-        }
-        return queryset.annotate(**annotation)
+        return queryset
 
     def get_perms_objects(self):
         return [self.get_project()]
