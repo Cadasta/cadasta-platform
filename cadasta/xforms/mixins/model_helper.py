a8c282f920398b8e7f80155a8eda674b3c6d053f
Eugene Alvin Villar <evillar@cadasta.org>
2018-01-02 05:14:35 +0800

    Fix resource file not being attached bug

--- cadasta/xforms/mixins/model_helper.py
+++ cadasta/xforms/mixins/model_helper.py
@@ -294,40 +294,17 @@ class ModelHelper():
              tenure_relationships, tenure_resources
              ) = self.create_models(submission, request.user)
 
-            party_submissions = [submission]
-            location_submissions = [submission]
-            tenure_submissions = [submission]
-
-            if 'party_repeat' in submission:
-                party_submissions = self._format_repeat(submission, ['party'])
-                if 'tenure_type' in party_submissions[0]:
-                    tenure_submissions = party_submissions
-
-            elif 'location_repeat' in submission:
-                location_submissions = self._format_repeat(
-                    submission, ['location']
-                )
-                if 'tenure_type' in location_submissions[0]:
-                    tenure_submissions = location_submissions
-
             party_resource_files = []
+            for party in party_resources:
+                party_resource_files.extend(party['resources'])
+
             location_resource_files = []
+            for location in location_resources:
+                location_resource_files.extend(location['resources'])
+
             tenure_resource_files = []
-
-            for group in party_submissions:
-                party_resource_files.extend(
-                    self._get_resource_files(group, 'party')
-                )
-
-            for group in location_submissions:
-                location_resource_files.extend(
-                    self._get_resource_files(group, 'location')
-                )
-
-            for group in tenure_submissions:
-                tenure_resource_files.extend(
-                    self._get_resource_files(group, 'tenure')
-                )
+            for tenure in tenure_resources:
+                tenure_resource_files.extend(tenure['resources'])
 
             resource_data = {
                 'project': questionnaire.project,
@@ -440,14 +417,6 @@ class ModelHelper():
                     else:
                         attributes[item] = data[attr_group][item]
         return attributes
-
-    def _get_resource_files(self, data, model_type):
-        resources = []
-        for file_name in data.keys():
-            if ("{}_resource".format(model_type) in file_name or
-               "{}_photo".format(model_type) in file_name):
-                resources.append(data[file_name])
-        return resources
 
     def _get_resource_names(self, data, model, model_type):
         group_name = '{}_attributes'.format(model_type)
