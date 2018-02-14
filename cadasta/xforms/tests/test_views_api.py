11f01832895a716e6a3dd59fcdb389557086e26b
Oliver Roick <oliver.roick@gmail.com>
2018-02-14 15:22:04 +0100

    Make Flake happy

--- cadasta/xforms/tests/test_views_api.py
+++ cadasta/xforms/tests/test_views_api.py
@@ -6,7 +6,6 @@ from lxml import etree
 
 from django.test import TestCase
 from django.contrib.contenttypes.models import ContentType
-from django.core.exceptions import ValidationError
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from skivvy import APITestCase
 from tutelary.models import Policy
