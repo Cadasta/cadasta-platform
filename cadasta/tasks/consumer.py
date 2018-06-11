fa529572bb3bbb83f7e3d89dd2805e28147bf681
Anthony Lukach <anthonylukach@gmail.com>
2018-06-11 09:26:05 -0600

    Reset DB connections if coononection is lost (#2041)

--- cadasta/tasks/consumer.py
+++ cadasta/tasks/consumer.py
@@ -1,6 +1,7 @@
 import logging
 
 import boto3
+from django.db import close_old_connections, OperationalError, InterfaceError
 from kombu.mixins import ConsumerMixin
 
 from tasks.celery import conf
@@ -26,19 +27,25 @@ class Worker(ConsumerMixin):
     def process_task(self, body, message):
         logger.info('Processing message: %r', message)
         try:
-            return self._handle_task(body, message)
+            self._handle_task(body, message)
+        except (OperationalError, InterfaceError):
+            # Lost DB connection, close DB and don't ack() msg.
+            # A new DB connection will be re-opened next time we
+            # try to access the DB. Msg will be re-processed
+            # after SQS visibility timeout passes.
+            logger.exception("DB connection lost. Cleaning up connections")
+            return close_old_connections()
         except:  # NOQA
-            logger.exception(
-                "Failed to process message: %r", message)
-        finally:
-            logger.info("ACKing message %r", message)
-            if self.connection.as_uri().lower().startswith('sqs://'):
-                # HACK: Can't seem to get message.ack() to work for SQS
-                # backend. Without this hack, messages will keep
-                # re-appearing after the visibility_timeout expires.
-                # See https://github.com/celery/kombu/issues/758
-                return self._sqs_ack(message)
-            return message.ack()
+            logger.exception("Failed to process message: %r", message)
+
+        logger.info("ACKing message %r", message)
+        if self.connection.as_uri().lower().startswith('sqs://'):
+            # HACK: Can't seem to get message.ack() to work for SQS
+            # backend. Without this hack, messages will keep
+            # re-appearing after the visibility_timeout expires.
+            # See https://github.com/celery/kombu/issues/758
+            return self._sqs_ack(message)
+        return message.ack()
 
     def _sqs_ack(self, message):
         logger.debug("Manually ACKing SQS message %r", message)
